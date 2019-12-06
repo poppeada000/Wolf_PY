9931213b7b3ce25134d286c397b63aaf90fd8ed1
Adam Popper <popadam123@gmail.com>
2019-12-06 13:20:01 -0600

    end of semester

--- buil_maps.py
+++ buil_maps.py
@@ -4,7 +4,12 @@ import matplotlib.pyplot as plt
 def getContent(file):
     return open('./src/content/'+file+'.txt', 'r').readlines()
 
-
+def setUpMap():
+    longMin
+    longMax
+    latMin
+    latMax
+    BBox = ((longMin,longMax,latMin,latMax)
 
 if __name__ == '__main__'   :
     getContent('./src/maps/mapdata.txt')
